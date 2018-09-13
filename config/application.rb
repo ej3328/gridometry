04b14d1e3b80c88cb7480b055d3276e71f1b2f1c
Everett Johnson <ej3328@gmail>
2018-09-13 16:32:05 -0700

    update

--- config/application.rb
+++ config/application.rb
@@ -3,17 +3,8 @@ require_relative 'boot'
 require 'rails/all'
 
 
- config_files = ['secrets.yml']
 
-    config_files.each do |file_name|
-      file_path = File.join(Rails.root, 'config', file_name)
-      config_keys = HashWithIndifferentAccess.new(YAML::load(IO.read(file_path)))[Rails.env]
-      config_keys.each do |k,v|
-        ENV[k.upcase] ||= v
-      end
-    end
 
-    
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
